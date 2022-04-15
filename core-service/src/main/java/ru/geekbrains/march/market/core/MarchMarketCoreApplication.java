7359f03f59611758469e425dcca0831411141d77
Aleksandr <gvardianez@yandex.ru>
2022-04-15 12:29:31 +0300

    Base project

---
+++ core-service/src/main/java/ru/geekbrains/march/market/core/MarchMarketCoreApplication.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.march.market.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarchMarketCoreApplication {
+	// Домашнее задание:
+	// 1. Разобраться с кодом
+	// 2. Реализовать создание заказов в кор-сервисе
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarchMarketCoreApplication.class, args);
+	}
+}
---
+++ core-service/src/main/java/ru/geekbrains/march/market/core/controllers/AuthController.java
@@ -0,0 +1,37 @@
+package ru.geekbrains.march.market.core.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+import ru.geekbrains.march.market.api.JwtRequest;
+import ru.geekbrains.march.market.api.JwtResponse;
+import ru.geekbrains.march.market.core.exceptions.AppError;
+import ru.geekbrains.march.market.core.services.UserService;
+import ru.geekbrains.march.market.core.utils.JwtTokenUtil;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+    private final UserService userService;
+    private final JwtTokenUtil jwtTokenUtil;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping
+    public ResponseEntity<?> createAuthToken(@RequestBody JwtRequest authRequest) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        } catch (BadCredentialsException e) {
+            return new ResponseEntity<>(new AppError("CHECK_TOKEN_ERROR", "Некорректный логин или пароль"), HttpStatus.UNAUTHORIZED);
+        }
+        UserDetails userDetails = userService.loadUserByUsername(authRequest.getUsername());
+        String token = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+}
